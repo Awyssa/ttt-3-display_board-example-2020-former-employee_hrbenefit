77952ea73ca7da74f2561f3f0c8d99a8567eabcc
Michael Ernest <m.hendersun@gmail.com>
2020-09-23 11:51:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,15 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+  def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+end
+
+display_board
